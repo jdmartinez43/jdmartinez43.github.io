ad066d9dd7edb3be44ba148ed28321dac76d7f15
Jose Martinez <josedmartinez43@gmail.com>
2021-08-04 00:13:39 -0700

    test prompt

---
+++ indiecesive.js
@@ -0,0 +1,7 @@
+// my first javascript app 
+// we shall figure out where to start after these commercial breaks
+
+
+let theNumber = Number(prompt("Pick a number"));
+console.log("Your number is the square root of " + theNumber * theNumber);
+
