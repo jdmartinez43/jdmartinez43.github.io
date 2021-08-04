81f5b02c9c462723c67d4a1a17f13fd30a221089
jdmartinez43 <josedmartinez43@gmail.com>
2021-08-04 10:31:39 -0700

    javascript if

--- indiecesive.js
+++ indiecesive.js
@@ -6,11 +6,12 @@ let n = Number(prompt("Pick a number"));
 
 // if n is not a number then
 if (!Number.isNaN(n)){
-    console.log("Your number is the square root of " + n * n);
-}else if  (typeof n == String){
     console.log("string not a number")
+}else if  (n > 10){
+    console.log("Your number is the square root of " + n * n);
+
 }else{
-    console.log('idk')
+    console.log('idk too msalls')
 }
 
 
